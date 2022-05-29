beda315d28d6d2dee07e1c336b39dc13a62ba21d
endiesworld <endiesworld@gmail.com>
2022-05-29 03:40:25 +0100

    Added input output functions

--- input_output/input_output.py
+++ input_output/input_output.py
@@ -1,1 +1,34 @@
 print('Hello Emmanuel, welcome to pyhton')
+
+
+def string_char_counter(arg, char):
+    print(
+        f"There are {str(arg).lower().count(str(char).lower())} of '{char}' in the {arg} ")
+    return None
+
+
+def keyboard_input_str(user_message='Type any message'):
+    format_user_message = user_message + '? '
+    user_input = input(format_user_message)
+    return user_input
+
+
+def keyboard_input_int(user_message='Type any message'):
+    format_user_message = user_message + ': '
+    user_input = input(format_user_message)
+    user_input = float((user_input))
+    if (user_input).is_integer():
+        user_input = int(user_input)
+    return user_input
+
+
+""" *** OUT PUT SECTION *** """
+
+# word = keyboard_input_str('Enter the any word')
+# char = keyboard_input_str('Enter the character you want to count')
+# string_char_counter(word, char)
+
+print(int(5/2))
+
+number = keyboard_input_int('Enter a number')
+print('Inputed value: ', number)
