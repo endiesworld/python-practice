709308119a0f27eebc81065b58c0658e217f4ddb
endiesworld <endiesworld@gmail.com>
2022-05-29 04:44:26 +0100

    writen only integer input

--- input_output/input_output.py
+++ input_output/input_output.py
@@ -1,3 +1,6 @@
+import re
+
+
 print('Hello Emmanuel, welcome to pyhton')
 
 
@@ -13,12 +16,24 @@ def keyboard_input_str(user_message='Type any message'):
     return user_input
 
 
-def keyboard_input_int(user_message='Type any message'):
+def keyboard_input_int(user_message='Enter only whole numbers'):
     format_user_message = user_message + ': '
-    user_input = input(format_user_message)
-    user_input = float((user_input))
-    if (user_input).is_integer():
-        user_input = int(user_input)
+    request_input = True
+
+    while(request_input):
+        user_input = input(format_user_message)
+
+        # [^ 0-9] means any non-digit character.
+        pattern = '[^0-9]'
+        check_input = re.search(pattern, user_input)
+
+        if not check_input:
+            request_input = False
+        else:
+            format_user_message = 'Please enter only integers: '
+
+    user_input = int(user_input)
+
     return user_input
 
 
@@ -28,7 +43,6 @@ def keyboard_input_int(user_message='Type any message'):
 # char = keyboard_input_str('Enter the character you want to count')
 # string_char_counter(word, char)
 
-print(int(5/2))
 
 number = keyboard_input_int('Enter a number')
 print('Inputed value: ', number)
