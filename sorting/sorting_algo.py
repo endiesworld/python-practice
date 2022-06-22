1056191ff25e13ec7cd0ef62b4f5ea7b77ba7799
endiesworld <endiesworld@gmail.com>
2022-06-22 07:28:49 +0100

    Added sorting algorithms

---
+++ sorting/sorting_algo.py
@@ -0,0 +1,16 @@
+# Bubble Sort
+"""
+    Each bubble up takes a full sweep through the list, swapping the adjacent elements, if they are in
+    the wrong order. The complexity for Bubble sort is O(n^2)
+"""
+
+# Insertionv Sort
+"""
+    The array is split into a sorted and unsorted part, values from the unsorted part are picked and 
+    placed at the correct position in the sorted part. The complexity is O(n^2)
+"""
+
+# Merge Sort
+"""
+
+"""
