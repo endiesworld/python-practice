04c8d21c592aeaa894ee9c017be9111b6bba856f
endiesworld <endiesworld@gmail.com>
2022-06-24 15:35:42 +0100

    Added asyncio operations

--- asyncio/asyncio_operations.py
+++ asyncio/asyncio_operations.py
@@ -3,6 +3,19 @@
     Asynchronous routines are routines that can be paused while waiting on their ultimate result, and let other routines run in the meantime.
     Asunchronous code, gives the look and feel of concurrency.
     It is used to provide high-performance network and web-servers, database connection libraries, distributed task queues, etc.
+    
+    # https://realpython.com/async-io-python/
+    The syntax async def introduces either a native coroutine or an asynchronous generator
+    
+    The keyword await passes function control back to the event loop. (It suspends the execution of the surrounding coroutine.) 
+    If Python encounters an await f() expression in the scope of g(), this is how await tells the event loop, 
+    “Suspend execution of g() until whatever I’m waiting on—the result of f()—is returned. In the meantime, go let something else run.”
+    
+    A function that you introduce with async def is a coroutine. It may use await, return, or yield, but all of these are optional.
+    You can only use await in the body of coroutines.
+    
+    When you use await f(), it’s required that f() be an object that is awaitable. 
+    awaitable object is either (1) another coroutine or (2) an object defining an .__await__() dunder method that returns an iterator
 """
 
 import asyncio
