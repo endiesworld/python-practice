f87e8b6ad51f7062e944843b13b0d2b1c995c855
Emmanuel <endiesworld@gmail.com>
2022-06-29 06:18:15 +0100

    Added update to binary search tree

--- search_algorithm/binary_search_tree.py
+++ search_algorithm/binary_search_tree.py
@@ -13,16 +13,31 @@ data = [-10, -3, 0, 5, 9]
 
 def processing(root):
     stack = [root]
-    print(stack)
 
     while (len(stack) > 0):
-        current = stack.pop()
-        if(current.left):
-            print(current.left)
-            stack.append(current.left)
-        if(current.right):
-            print(current.right)
+        current = stack.pop()  # pop the node at the top of the stack out
+        print(current.value)
+        # current.left points to the left node
+        #  .value refers to the value in the node
+        # current.right points to the left node
+        #  .value refers to the value in the node
+        # Adds the right node to stack
+        if (current.right):
             stack.append(current.right)
+
+        if (current.left):
+            stack.append(current.left)
+    return None
+
+
+def bst_ops(arr):
+    mid = len(arr) // 2
+    root = Node(arr[mid])
+    left_side = arr[:mid]
+    right_side = arr[mid+1:]
+    counter = -1
+    while counter < mid:
+        pass
 
 
 root = Node(0)
@@ -33,7 +48,7 @@ node_R_2 = Node(5)
 
 root.left = node_L_1
 root.right = node_R_1
-node_L_1.right = node_L_2
-node_R_1.right = node_R_2
+node_L_1.left = node_L_2
+node_R_1.left = node_R_2
 
 processing(root)
