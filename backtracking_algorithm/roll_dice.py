fe7ab02c879a807f3cdb2702ac4b7e3d47126faa
endiesworld <endiesworld@gmail.com>
2022-07-14 08:40:14 +0100

    Started backtracking project

--- backtracking_algorithm/roll_dice.py
+++ backtracking_algorithm/roll_dice.py
@@ -1,9 +1,16 @@
-def roll_dice(dice_number):
-    if dice_number == 0:
+# Yet to finish this
+def roll_dice(dice):
+    store = []
+    return _roll_dice(dice, store)
+
+
+def _roll_dice(dice, rolled_dice):
+    if dice == 0:
         return ''
     store = []
+    dice_number = dice
     for number in range(1, 7):
-        data = roll_dice(dice_number - 1)
+        data = _roll_dice(dice - 1)
         dice_val = '{},{}'.format(data, number)
         store.append((dice_val))
     return store
