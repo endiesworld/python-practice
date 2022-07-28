7e9d822455d296ba84109c983ff57939eb0df8d9
Emmanuel <endiesworld@gmail.com>
2022-07-28 04:10:01 +0100

    Added final code for iterable and iterator

--- fundamentals/iteration.py
+++ fundamentals/iteration.py
@@ -4,52 +4,29 @@
 """
 
 
-def next_value(iterable):
-    try:
-        return next(iterable)
-    except StopIteration:
-        raise ValueError('End of sequence reached')
-
-
 def iterator_func(sequence):
-    return iter(sequence)
+    iterable = iter(sequence)
+
+    def next_value():
+        try:
+            return next(iterable)
+        except StopIteration:
+            raise ValueError('End of sequence reached')
+
+    return next_value
 
 
-caller = iterator_func([2, 4, 6, 8])
-print(next(caller))
-print(next(caller))
-# # def iterator_func(sequence):
-# #     iterable = iter(sequence)
-# #     try:
-# #         return next(iterable)
-# #     except StopIteration:
-# #         # return 'End of sequence reached'
-# #         raise ValueError('End of sequence reached')
+list_collection = [2, 4, 6, 8]
+set_collection = {2, 4, 6, 8}
+list_caller = iterator_func(list_collection)
+set_caller = iterator_func(set_collection)
 
-
-# list_collection = [2, 4, 6, 8]
-# set_collection = {2, 4, 6, 8}
-# list_iterable = iter(list_collection)
-# set_iterable = iter(set_collection)
-
-
-# def get_next_list_val():
-#     global list_iterable
-#     try:
-#         return next(list_iterable)
-#     except StopIteration:
-#         # return 'End of sequence reached'
-#         raise ValueError('End of sequence reached')
-
-
-# print(get_next_list_val())
-# print(get_next_list_val())
-# print(get_next_list_val())
-# print(get_next_list_val())
-# print(get_next_list_val())
-# for position, value in enumerate(list_collection):
-#     print(
-#         f'Element value in position {position + 1 } in the list sequence: {iterator_func(list_collection)}')
-
-#     print(
-#         f'Element value in position {position + 1 } in the list sequence: {iterator_func(set_collection)}')
+print(list_caller())
+print(list_caller())
+print(list_caller())
+print(list_caller())
+print('================= SET_CALLER =============')
+print(set_caller())
+print(set_caller())
+print(set_caller())
+print(set_caller())
