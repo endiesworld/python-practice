f8ac8c381ec5ae1e80d3c7386095fca18f51b7bc
endiesworld <endiesworld@gmail.com>
2022-11-07 03:01:43 +0100

    Added animal class for inheritancetest

---
+++ classes/animal_class.py
@@ -0,0 +1,37 @@
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def Animal_details(self):
+        print('Name: ', self.name)
+        print('Sound: ', self.sound)
+    # write your class here
+
+
+class Dog(Animal):
+    def __init__(self, name, sound, family=None):
+        super().__init__(name, sound)
+        self.family = family
+
+    def Animal_details(self):
+        super().Animal_details()
+        print('family: ', self.family)
+
+
+class Sheep(Animal):
+    def __init__(self, name, sound, color=None):
+        super().__init__(name, sound)
+        self.color = color
+
+    def Animal_details(self):
+        super().Animal_details()
+        print('family: ', self.color)
+   # write your class here
+
+
+d = Dog("Pongo", "Woof Woof", "Husky")
+d.Animal_details()
+print(" ")
+s = Sheep("Billy", "Baaa Baaa", "White")
+s.Animal_details()
