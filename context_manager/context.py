ee9e893ffff0d2287715830430b16e1b40d80897
endiesworld <endiesworld@gmail.com>
2024-06-12 07:08:59 +0100

    feat: context manager ops with user defined class

---
+++ context_manager/context.py
@@ -0,0 +1,42 @@
+"""
+    Context managers provide a clean interface for obaining and releasing resources.
+    To do this, we need to add the magic method __enter__ and __exit__ to the class of function.
+"""
+import time
+
+from typing import Any
+from random import sample
+from string import ascii_letters
+from pathlib import Path
+
+
+class TempFile:
+    def __init__(self, filename=None) -> None:
+        if not filename:
+            filename = "".join(sample(ascii_letters, 10))
+        self.file = Path(filename)
+        print(f"The current file path is: {self.file}")
+        print(f"Does {self.file} exist:{self.file.is_file()}")
+        
+    def __enter__(self):
+        self.file.parent.mkdir(parents=True, exist_ok=True)
+        
+        #I file exist, delete it
+        if self.file.exists():
+            self.file.unlink()
+            
+        # Recreate the file
+        self.file.touch()
+        return self.file.open("w")
+        
+    def __exit__(self, *args):
+        
+        self.file.unlink()    
+        
+        
+with TempFile() as tf:
+    tf.write(" This is a tempfile context manager test\n File will be deleted shortly\n")
+    tf.flush()
+    time.sleep(20)
+        
+    
--- context_manager/simple_context_manager.py
+++ context_manager/simple_context_manager.py
@@ -7,13 +7,21 @@ import time
 def write_to_file(filename=None):
     filename: str = filename
     if not filename:
-        filename = "".join(sample(ascii_letters, 15))
+        filename = "".join(sample(ascii_letters, 15)) + '.txt'
+        
+    concrete_file = Path.cwd().joinpath(filename)
+    concrete_file.touch()
     
-    with open(filename, 'w') as f:
-        f.write('Hello, world')
+    with open(concrete_file, 'w') as f:
+        f.write('Hello, world\n')
         f.write('This random file will be deleted in 20secs.')
         f.flush()
-        time.sleep(21)
+        time.sleep(5)
+        
+    with open(concrete_file, 'r') as f:
+        print(f.readlines())
+        
+    concrete_file.unlink()
 
 if __name__ == '__main__':
     write_to_file()
