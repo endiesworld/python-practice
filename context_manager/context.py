9f05be5ba1971fef64a73bed00ae93b629a9a7f8
endiesworld <endiesworld@gmail.com>
2024-08-02 04:01:13 +0100

    Added exception handling technique to context manager

--- context_manager/context.py
+++ context_manager/context.py
@@ -12,31 +12,66 @@ from pathlib import Path
 
 class TempFile:
     def __init__(self, filename=None) -> None:
+        print("Entering init...")
         if not filename:
             filename = "".join(sample(ascii_letters, 10))
             filename = filename + '.txt'
         self.file = Path(filename)
+        self.file_handle = None  # Initialize the file handle to None
         print(f"The current file path is: {self.file}")
         print(f"Does {self.file} exist:{self.file.is_file()}")
         
     def __enter__(self):
+        print("Entering __enter__...")
+        print(f"The current self.file.parent path is: {self.file.parent}")
+        print(f"Does self.file.parents exist:{self.file.parents}")
         self.file.parent.mkdir(parents=True, exist_ok=True)
         
-        #I file exist, delete it
+        #If file exist, delete it
         if self.file.exists():
             self.file.unlink()
             
-        # Recreate the file
+        # Recreate the file and open it
         self.file.touch()
         return self.file.open("w")
         
+    
     def __exit__(self, *args):
-        self.file.unlink()    
+        print("Entering __exit__...")
+        self.file.unlink()
+        # retries = 5
+        # while retries > 0:
+        #     try:
+        #         self.file.unlink()
+        #         break
+        #     except PermissionError:
+        #         retries -= 1
+        #         time.sleep(0.5)  # Wait half a second before retrying
+        #     if retries == 0:
+        #         print(f"Failed to delete {self.file} after multiple attempts.")
         
         
-with TempFile() as tf:
-    tf.write(" This is a tempfile context manager test\n File will be deleted shortly\n")
-    tf.flush()
-    time.sleep(20)
+# with TempFile() as tf:
+#     tf.write(" This is a tempfile context manager test\n File will be deleted shortly\n")
+#     tf.flush()
+#     time.sleep(2)
+        
+
+class Except:
+    def __init__(self, *exc_types, message=None):
+        self.exc_types = exc_types
+        self.message = message
+        
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_message, exc_trace_back):
+        if exc_type in self.exc_types:
+            if self.message and self.message in exc_message.args:
+                return True
+            elif self.message:
+                return
+            return True
+        raise Exception(f"Expected one of {self.exc_types} Exceptions.")
         
     
