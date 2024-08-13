64728d3197a09668dba602d4273e3fc26197cf13
endiesworld <endiesworld@gmail.com>
2024-08-13 15:47:38 +0100

    feat: Added encapsulation for adding data to mysql database

--- databases/Mysql_database/encapsulation.py
+++ databases/Mysql_database/encapsulation.py
@@ -9,6 +9,7 @@ HOST=os.getenv('DATABASE_URL')
 USER=os.getenv('DATABASE_USER')
 SCHEMA=os.getenv('SCHEMA')
 
+
 class Database:
     def __init__(self, host, username, password, database):
         self.host = host
@@ -28,6 +29,7 @@ class Database:
         return self.cursor
     
     def __exit__(self, exc_type, exc_value, trace):
+        self.conn.commit()
         self.conn.close()
         
 
@@ -38,6 +40,25 @@ def read_data(table_name):
         projet_records = cursor.fetchall()
         print(projet_records)
         
+        
+def add_project(project_datas=[]):
+    last_row_id = None
+    with Database(HOST, USER, PASSWORD, SCHEMA) as cursor:
+        for project_data in project_datas:
+            query = """ INSERT INTO projects(title, description) VALUES (%s, %s) """
+            cursor.execute(query, project_data)
+        last_row_id = cursor.lastrowid
+    return last_row_id
+
+def add_tasks(project_id, tasks=[]):
+    tasks_data = []
+    with Database(HOST, USER, PASSWORD, SCHEMA) as cursor:
+        for task in tasks:
+            task_data = (project_id, task)
+            tasks_data.append(task_data)
+        query = """ INSERT INTO tasks(project_id, description) VALUES (%s, %s) """
+        cursor.executemany(query, tasks_data)
+
 
 def read_projects():
     read_data("projects")
@@ -48,5 +69,9 @@ def read_task():
 
     
 if __name__ == "__main__":
-    read_projects()
-    read_task()
+    TASK = ["Clean bathroom", "Clean kitchen", "Clean living room"]
+    projects = [("Clean house", "Clean house by room")]
+    # read_projects()
+    # read_task()
+    project_id = add_project(projects)
+    add_tasks(project_id, TASK)
