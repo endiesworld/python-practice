4e591b445315538e2db3d2f8815de817267cbbb8
endiesworld <endiesworld@gmail.com>
2024-10-08 23:10:32 -0400

    Added subsetting of n-dimensional numpy array

--- data-fundamentals/numpy-intro.py
+++ data-fundamentals/numpy-intro.py
@@ -44,3 +44,10 @@ print(type(np_weight)) # print <class 'numpy.ndarray'> the ndarray stands for n-
 np_2d = np.array([[1.73, 1.68, 1.71, 1.89, 1.79], [65.4, 59.2, 63.6, 88.4, 68.7]])
 
 print("To view the shape of a numpy array: ", np_2d.shape)
+
+# Subsetting
+print(np_2d[0]) # array([[1.73, 1.68, 1.71, 1.89, 1.79])
+print(np_2d[0][2]) # 1.71
+print(np_2d[0, 2]) # 1.71
+print(np_2d[:, 1:3]) # ":" means take all rows "1:3" means start from the second column but stop at the forth column and do not include the forth column. array([[ 1.68,  1.71],  [59.2 , 63.6 ]])
+print(np_2d[1, :]) # Take the second row and print everything in it. array([65.4, 59.2, 63.6, 88.4, 68.7])
