5b8a444e9bdb99e926382b43ffe849bb789da237
Emmanuel <endiesworld@gmail.com>
2024-10-16 13:31:36 -0400

    local update

---
+++ input_output/assignment-2-540.py
@@ -0,0 +1,9 @@
+"""
+Using conditionals (if-then-else) and exception-handling
+
+Your code should accept the (unsorted) first and last names of 9 students and then randomly combine sets of three students into the same group. Then, your code should display the three groups of students.
+
+To make sure the student assignment is random, we apply a simple rule. If students' names are to be sorted based on last names, then any two students following each other in the sorted array cannot be in the same group.  
+
+Please submit the .py file(s) when done
+"""
